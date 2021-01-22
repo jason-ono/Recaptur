fac102b59ee1857544f244ef0dbcfd2f6e2beb6f
jason-ono <exia1999@gmail.com>
2021-01-21 19:50:45 -0500

    Initial Commit

---
+++ Captur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Captur
+//
+//  Created by Kotaro Ono on 2021/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
